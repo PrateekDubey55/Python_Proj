bb243ca1fd1155ffb212d8e03a1460c33260e259
System1 <prateek.dubeyindore@gmail.com>
2021-02-13 16:00:34 +0530

    Added functions

--- test2.py
+++ test2.py
@@ -15,98 +15,106 @@ record_dictionary = {
         'Contact': 4567891230
     }
 }
-while True:
-    cmd_input = input("\n1. Insert \t2. Update \t3. Delete \t4. Retrieve All \t5. Enter Name to find data\nEnter a command: ")
-    if cmd_input == '1':
-        name = input("Enter Name: ")
-        age = int(input("Enter Age: "))
-        contact = int(input("Enter Contact Number: "))
-        i = len(record_dictionary) + 1
-        record_dictionary[i] = {}
-        record_dictionary[i]['Name'] = name.title()
-        record_dictionary[i]['Age'] = age
-        record_dictionary[i]['Contact'] = contact
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
-    elif cmd_input == '2':
-        flag = 0
-        name = input("Enter Name to update: ").title()
-        status = input("Enter to update \t1.Name\t2. Age\t3.Contact\n")
-        if status == '1':
-            new_Name = input("Enter New Name: ")
-            for s_id,s_info in record_dictionary.items():
-                for key,value in s_info.items():
-                    if value == name:
-                        flag = 1
-                        record_dictionary[s_id]['Name'] = new_Name
-                        print("Record Updated")
-                        break
-            if flag == 0:
-                print("No Record Found!!!")
-        elif status == '2':
-            new_Age = input("Enter New Age: ")
-            for s_id,s_info in record_dictionary.items():
-                for key,value in s_info.items():
-                    if value == name:
-                        flag = 1
-                        record_dictionary[s_id]['Age'] = new_Age
-                        print("Record Updated")
-                        break
-            if flag == 0:
-                print("No Record Found!!!")
-        elif status == '3':
-            new_Contact = input("Enter New Contact: ")
-            for s_id,s_info in record_dictionary.items():
-                for key,value in s_info.items():
-                    if value == name:
-                        flag = 1
-                        record_dictionary[s_id]['Contact'] = new_Contact
-                        print("Record Updated")
-                        break
-            if flag == 0:
-                print("No Record Found!!!")
-        else: 
-            print("WRONG INPUT!!!")
-            break
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
-    elif cmd_input == '3':
-        flag = 0
-        name = input("Enter Name to delete: ").title()
-        record_dictionary1 = record_dictionary.copy()
-        for s_id,s_info in record_dictionary1.items():
-            for key,value in s_info.items():
-                if value == name:
-                    flag = 1
-                    del(record_dictionary[s_id])
-                    print("Record deleted")
-                    # record_dictionary = record_dictionary1.copy()
-                    break
-        if flag == 0:
-            print("No Record Found!!!")
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
-    elif cmd_input == '4':
-        for s_id, s_info in record_dictionary.items():
-            print("\nStudent ID: ",s_id)
-            for key,value in s_info.items():
-                print(key + ':',value)
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
-    elif cmd_input == '5':
-        flag = 0
-        name = input("Enter Name: ").title()
+
+def insert(name, age, contact, i):
+    record_dictionary[i] = {}
+    record_dictionary[i]['Name'] = name.title()
+    record_dictionary[i]['Age'] = age
+    record_dictionary[i]['Contact'] = contact
+
+def update(name, status):
+    flag = 0
+    if status == '1':
+        new_Name = input("Enter New Name: ")
         for s_id,s_info in record_dictionary.items():
             for key,value in s_info.items():
                 if value == name:
                     flag = 1
-                    print(record_dictionary[s_id])
+                    record_dictionary[s_id]['Name'] = new_Name
+                    print("Record Updated")
                     break
-        if flag == 0: 
+        if flag == 0:
             print("No Record Found!!!")
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
-    else:
-        print("Invalid Input!")
-        i = input("Press 0 to exit or any other key to conitnue\n")
-        if(i == '0') : break
+    elif status == '2':
+        new_Age = input("Enter New Age: ")
+        for s_id,s_info in record_dictionary.items():
+            for key,value in s_info.items():
+                if value == name:
+                    flag = 1
+                    record_dictionary[s_id]['Age'] = new_Age
+                    print("Record Updated")
+                    break
+        if flag == 0:
+            print("No Record Found!!!")
+    elif status == '3':
+        new_Contact = input("Enter New Contact: ")
+        for s_id,s_info in record_dictionary.items():
+            for key,value in s_info.items():
+                if value == name:
+                    flag = 1
+                    record_dictionary[s_id]['Contact'] = new_Contact
+                    print("Record Updated")
+                    break
+        if flag == 0:
+            print("No Record Found!!!")
+    else: 
+        print("WRONG INPUT!!!")
+
+def deleteData(name):
+    flag = 0
+    record_dictionary1 = record_dictionary.copy()
+    for s_id,s_info in record_dictionary1.items():
+        for key,value in s_info.items():
+            if value == name:
+                flag = 1
+                del(record_dictionary[s_id])
+                print("Record deleted")
+                break
+    if flag == 0:
+        print("No Record Found!!!")
+
+def retrieve(name):
+    flag = 0
+    for s_id,s_info in record_dictionary.items():
+        for key,value in s_info.items():
+            if value == name:
+                flag = 1
+                print(record_dictionary[s_id])
+                break
+    if flag == 0: 
+        print("No Record Found!!!")
+
+def retrieveAll():
+    for s_id, s_info in record_dictionary.items():
+        print("\nStudent ID: ",s_id)
+        for key,value in s_info.items():
+            print(key + ':',value)
+        
+def main():
+    while True:
+        cmd_input = input("\n0.Exit 1.Insert 2.Update 3.Delete 4.Retrieve All 5.Enter Name to find data\nEnter a command: ")
+        if cmd_input == '0':
+            break
+        if cmd_input == '1':
+            name = input("Enter Name: ")
+            age = int(input("Enter Age: "))
+            contact = int(input("Enter Contact Number: "))
+            i = len(record_dictionary) + 1
+            insert(name, age, contact, i)
+        if cmd_input == '2':
+            name = input("Enter Name to update: ").title()
+            status = input("Enter to update \t1.Name\t2. Age\t3.Contact\n")
+            update(name, status)
+        if cmd_input == '3':
+            name = input("Enter Name to delete: ").title()
+            deleteData(name)
+        if cmd_input == '4':
+            retrieveAll()
+        if cmd_input == '5':
+            name = input("Enter Name: ").title()
+            retrieve(name)
+        else:
+            print("Invalid Input!!!")
+
+if __name__ == '__main__':
+    main()
